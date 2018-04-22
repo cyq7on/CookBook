06fdd2fdee9231ccafa9862760662260eb782195
cyq7on <cyq7on@qq.com>
2018-04-22 22:51:26 +0800

    add project

---
+++ app/src/main/java/com/cyq7on/cookbook/bean/User.java
@@ -0,0 +1,97 @@
+package com.cyq7on.cookbook.bean;
+
+import com.cyq7on.cookbook.db.NewFriend;
+import cn.bmob.v3.BmobUser;
+
+/**
+ * @author :smile
+ * @project:User
+ * @date :2016-01-22-18:11
+ */
+public class User extends BmobUser {
+
+    private String avatar;
+    private String age;
+    private int sex;
+    private int role;
+    private int depId;
+    //科室
+    private String department;
+    //病历
+    private String record;
+
+    public User(){}
+
+    public User(NewFriend friend){
+        setObjectId(friend.getUid());
+        setUsername(friend.getName());
+        setAvatar(friend.getAvatar());
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public String getRecord() {
+        return record;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getDepId() {
+        return depId;
+    }
+
+    public void setDepId(int depId) {
+        this.depId = depId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "avatar='" + avatar + '\'' +
+                ", age='" + age + '\'' +
+                ", sex=" + sex +
+                ", role=" + role +
+                ", depId=" + depId +
+                ", department='" + department + '\'' +
+                ", record='" + record + '\'' +
+                '}';
+    }
+}
