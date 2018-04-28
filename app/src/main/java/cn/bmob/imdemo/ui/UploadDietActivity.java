06319572c3bad3a35cae917f11b3359c2c474cb6
cyq7on <cyq7on@qq.com>
2018-04-28 21:57:51 +0800

    add upload dietplan activity

--- app/src/main/java/cn/bmob/imdemo/ui/UploadDietActivity.java
+++ app/src/main/java/cn/bmob/imdemo/ui/UploadDietActivity.java
@@ -1,11 +1,29 @@
 package cn.bmob.imdemo.ui;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.bmob.imdemo.R;
 import cn.bmob.imdemo.base.ParentWithNaviActivity;
 
 public class UploadDietActivity extends ParentWithNaviActivity {
+
+    @Bind(R.id.et_name)
+    EditText etName;
+    @Bind(R.id.et_breakfast)
+    EditText etBreakfast;
+    @Bind(R.id.et_launch)
+    EditText etLaunch;
+    @Bind(R.id.et_dinner)
+    EditText etDinner;
+    @Bind(R.id.et_nutrient)
+    TextView etNutrient;
+    @Bind(R.id.btn_upload)
+    Button btnUpload;
 
     @Override
     protected String title() {
@@ -16,5 +34,6 @@ public class UploadDietActivity extends ParentWithNaviActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_diet);
+        ButterKnife.bind(this);
     }
 }
